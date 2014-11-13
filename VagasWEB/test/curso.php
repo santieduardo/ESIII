792209b7f6e65ae297d6d7160c898c70e84bf04e
Henrique Rieger <henrique.rieger@gmail.com>
2014-11-13 11:02:16 -0200

    Teste Vagas publicas

---
+++ VagasWEB/test/curso.php
@@ -0,0 +1,20 @@
+<?php 
+class Curso {
+
+	private $idCurso;
+	private $curso;
+	
+	public function __construct($id = 0, $nome = null){
+		$this->idCurso = $id;
+		$this->curso = $curso;
+	}
+	
+	public function getId(){
+		return $this->idCurso;
+	}
+
+	public function getCurso(){
+		return $this->curso;
+	}
+}
+?>
