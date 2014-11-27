f0f5df34f5713c01e9101a505f83d8a13f2bd4df
Mariomrp <marioprederigo@gmail.com>
2014-11-27 11:22:15 -0200

    Update testeVagaDisponibilidade.php
    Inclusão do município na function

--- VagasWEB/test/testeVagaDisponibilidade.php
+++ VagasWEB/test/testeVagaDisponibilidade.php
@@ -1,1 +1,28 @@
-<?php	require_once "cidade.php";	require_once "curso.php";	require_once "usuario.php";	require_once "vaga.php";	class testeVagaDisponibilidade extends PHPUnit_Framework_Testcase{				public function testeVagaDisponivel() {				$vaga = new Vaga($idVaga=1, $nome="Desenvolvedor PHP", $descricao="Auxiliar no desenvolvimento de novas aplicações", $status=1, $turno="manhã", $publico=0);								$status = $vaga->getStatus();				$resposta = $vaga->checkStatus($status);				$this->assertTrue($resposta);								$vaga->setStatus(0);				$status = $vaga->getStatus();				$resposta = $vaga->checkStatus($status);				$this->assertFalse($resposta);								$vaga->setStatus("ola");				$status = $vaga->getStatus();				$resposta = $vaga->checkStatus($status);				$this->assertFalse($resposta);		}	}?>
+<?php
+
+	require_once "cidade.php";
+	require_once "curso.php";
+	require_once "usuario.php";
+	require_once "vaga.php";
+
+	class testeVagaDisponibilidade extends PHPUnit_Framework_Testcase{
+		
+		public function testeVagaDisponivel() {
+				$vaga = new Vaga($idVaga=1, $nome="Desenvolvedor PHP", $descricao="Auxiliar no desenvolvimento de novas aplicaÃ§Ãµes", $status=1, $turno="manhÃ£", $publico=0, $municipio = "Porto Alegre");
+				
+				$status = $vaga->getStatus();
+				$resposta = $vaga->checkStatus($status);
+				$this->assertTrue($resposta);
+				
+				$vaga->setStatus(0);
+				$status = $vaga->getStatus();
+				$resposta = $vaga->checkStatus($status);
+				$this->assertFalse($resposta);
+				
+				$vaga->setStatus("ola");
+				$status = $vaga->getStatus();
+				$resposta = $vaga->checkStatus($status);
+				$this->assertFalse($resposta);
+		}
+	}
+?>
