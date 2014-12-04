034fc57bfba3fb264f9d9ae237cf51675f3a89ca
Henrique Rieger <henrique502@users.noreply.github.com>
2014-12-04 09:25:25 -0200

    Create autoload.php

---
+++ VagasWEB/autoload.php
@@ -0,0 +1,29 @@
+<?php
+chdir(__DIR__);
+ini_set('include_path', __DIR__);
+
+function autoload($names, $path = "./"){
+	if(is_array($names)){
+		foreach($names as $file){
+			$class = (basename($file ,".php"));
+			require $path . lcfirst($class) . ".php";
+			
+			if(!class_exists($class, false))
+				trigger_error("Falha ao carregar a classe: $class", E_USER_WARNING);
+		}
+	} else {
+		$class = (basename($names ,".php"));
+		require $path . lcfirst($names) . ".php";
+			
+		if(!class_exists($class, false))
+			trigger_error("Falha ao carregar a classe: $class", E_USER_WARNING);
+	}
+}
+
+/* Classes */
+$models = array("vaga", "cidade", "curso", "usuario", "classeBD");
+$testes = glob(PATH . "/test/test*.php");
+
+/* Autoload */
+autoload($models);
+autoload($testes);
